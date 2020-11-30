bb24800ea7e395f609c60f5d533fc40bec5b705b
Sebastian Ozimkowski <sebastianozimkowski@outlook.com>
2020-11-30 09:57:49 +0100

    2

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,37 +2,33 @@
 # Simple shell script to install all you need to start using ubuntu for development
 
 POSTGRES_ROOT_PASSWORD=Kutas123
+USER_EMAIL=sebastianozimkowski@outlook.com
+USER_NAME="Sebastian Ozimkowski"
 
 # Add docker repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-var=apt-cache madison docker-ce | sort -rV | head -n 1 | awk 'BEGIN{FS="|";}{print $2}' | tr -d ' '
-
-# Add repositories of databases (MySQL, PostgreSQL)
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+var=$(apt-cache madison docker-ce | sort -rV | head -n 1 | awk 'BEGIN{FS="|";}{print $2}' | tr -d ' ')
 
 # Update/Upgrade system
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Install all core/usefull packages
-xargs -a packages.txt sudo apt-get -y install
+xargs -a ubuntu-packages.txt sudo apt-get -y install
 
 # Install Docker / Docker-compose
-sudo apt-get install docker-ce="$var" docker-ce-cli="$var" containerd.io
+sudo apt-get install docker-ce=$var docker-ce-cli=$var containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# Update pip
-pip3 install --upgrade pip
-
-# Add code folder
-mkdir ~/Code
+# Update pip, install python modules
+sudo pip3 install --upgrade pip
+sudo pip3 install -r python-modules-global.txt
 
 # Config git
-git config --global user.email "sebastianozimkowski@outlook.com"
-git config --global user.name "Sebastian Ozimkowski"
+git config --global user.email $USER_EMAIL
+git config --global user.name $USER_NAME
 
 # Install Visual Studio Code extensions
 code --install-extension ritwickdey.liveserver
@@ -41,19 +37,16 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension ms-python.python
 code --install-extension 2gua.rainbow-brackets
 code --install-extension wscats.eno
-code --install-extension scode-icons-team.vscode-icons
+code --install-extension vscode-icons-team.vscode-icons
 
 # Install nvidia drivers
 sudo ubuntu-drivers autoinstall
 
 # Configure databases 
-sudo -u postgres psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWOR '$POSTGRES_ROOT_PASSWORD';"
-sudo -u postgres psql -U postgres -d postgres -c "CREATE USER sebcio03 WITH PASSWORD '$POSTGRES_ROOT_PASSWORD';"
+sudo -u postgres psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD '$POSTGRES_ROOT_PASSWORD';"
+sudo -u postgres psql -U postgres -d postgres -c "CREATE USER user03 WITH PASSWORD '$POSTGRES_ROOT_PASSWORD';"
 sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE base;"
-sudo -u postgres psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE base TO sebcio03;"
+sudo -u postgres psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE base TO user03;"
 
-export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpw"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpw"
-sudo apt-get install -y 
-mysql_secure_installation
+# Add code folder
+sudo mkdir ~/Code
